b314fdd9dfc45b08c0963f4c34db7c9d45f7e621
emr3rden <emreerden@pm.me>
2022-11-02 21:49:06 +0300

    Fonts Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Caveat': ['Caveat', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
